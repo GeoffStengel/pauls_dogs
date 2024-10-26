63b9830354c8b526ae832fb1d7ca8448a01495be
unknown <geoffsglass@hotmail.com>
2024-10-25 22:07:57 -0800

    Pdated more UI on dashboard, index pages, and a bunch of stuff. Adding more notes and cleaning up code along the way. still a bug to fix in the forms again

--- pd_proj/settings.py
+++ pd_proj/settings.py
@@ -117,7 +117,6 @@ USE_TZ = True
 # Environment-based Static/Media File Handling
 ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
 
-
 if ENVIRONMENT == 'development':
     # Development settings for static and media files
     STATIC_URL = '/static/'
