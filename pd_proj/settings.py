081d42c27ec1131de070215843782a90dca80071
unknown <geoffsglass@hotmail.com>
2024-10-30 21:40:23 -0800

    Its uploading and creating new folder and files on upload_to in items page in the field called image, to local development, but not to aws still it should create the folder as well on its own.  So i think theres two possibilities either aws permissions or in the if statement on settings, on else statement in the production mode

--- pd_proj/settings.py
+++ pd_proj/settings.py
@@ -145,6 +145,12 @@ elif ENVIRONMENT_CONDITIONS == 'production':
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+    # Additional settings (optional)
+    #AWS_S3_OBJECT_PARAMETERS = {
+    #    'CacheControl': 'max-age=86400',
+    #}
+
+
     DATABASES = {
         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
