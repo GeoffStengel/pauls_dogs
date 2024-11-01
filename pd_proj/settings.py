6426b694d3f3ee2d47a5bcd542b17369eafd95a5
unknown <geoffsglass@hotmail.com>
2024-10-31 18:02:37 -0800

    Adjusted models to load media before ppp folder, switched default_file_storag back to storages.backends.s3boto3.S3Boto3Storage

--- pd_proj/settings.py
+++ pd_proj/settings.py
@@ -126,7 +126,7 @@ elif ENVIRONMENT_CONDITIONZ == 'production':
     AWS_REGION = os.environ.get('AWS_REGION', 'us-east-2')
 
     STATICFILES_STORAGE = 'pd_proj.storage.custom_storage.StaticStorage'
-    DEFAULT_FILE_STORAGE = 'pd_proj.storage.custom_storage.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
